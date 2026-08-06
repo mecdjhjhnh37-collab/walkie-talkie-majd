c7068263f2874e8f2f86f65c34623eee0630a610
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 01:58:06 +0300

    Refactor user.js to remove Firebase config
    Removed Firebase configuration and initialization from user.js.

--- static/user.js
+++ static/user.js
@@ -1,27 +1,16 @@
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBrxgm0VQTfRv0ixXh9uQ81HBJ8SmD8c1E",
-    authDomain: "mecd-voice-ap.firebaseapp.com",
-    projectId: "mecd-voice-ap",
-    storageBucket: "mecd-voice-ap.firebasestorage.app",
-    messagingSenderId: "595103941809",
-    appId: "1:595103941809:web:e50577f156bcea3633ce90"
-};
-
-
-const app = initializeApp(firebaseConfig);
+import { app } from "./app.js";
 
 const auth = getAuth(app);
 
 
-
 onAuthStateChanged(auth, (user)=>{
 
+
     if(user){
+
 
         document.getElementById("userName").textContent =
         user.displayName;
@@ -34,6 +23,12 @@ onAuthStateChanged(auth, (user)=>{
         document.getElementById("userId").textContent =
         "ID : " + user.uid;
 
+
+    } else {
+
+        console.log("لا يوجد مستخدم");
+
     }
+
 
 });
