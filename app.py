58de8f8baa413a9a48d807568b27594556f05f2a
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-06 17:57:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-import os
+@app.route("/login/ar")
+def login_ar():
+    return render_template("login_ar.html")
+
+
+@app.route("/login/tr")
+def login_tr():
+    return render_template("login_tr.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
