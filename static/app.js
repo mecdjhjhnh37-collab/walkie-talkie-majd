ecba95f09a44d873caa707a1f504e667f9fc6e38
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-06 20:57:50 +0300

    Create app.js

---
+++ static/app.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrxgm0VQTfRv0ixXh9uQ81HBJ8SmD8c1E",
+  authDomain: "mecd-voice-ap.firebaseapp.com",
+  projectId: "mecd-voice-ap",
+  storageBucket: "mecd-voice-ap.firebasestorage.app",
+  messagingSenderId: "595103941809",
+  appId: "1:595103941809:web:08af004c43bc94ec33ce90"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+
+document.getElementById("googleLogin").innerHTML = `
+<button id="loginBtn">
+🔵 تسجيل الدخول بواسطة Google
+</button>
+`;
+
+document.getElementById("loginBtn").onclick = async () => {
+
+alert("تم الضغط على الزر");
+
+try {
+
+
+    const result = await signInWithPopup(auth, provider);
+
+    const user = result.user;
+
+    alert("مرحباً " + user.displayName);
+
+    console.log(user);
+
+  } catch(error) {
+
+    console.log(error);
+    alert("حدث خطأ في تسجيل الدخول");
+
+  }
+
+};
