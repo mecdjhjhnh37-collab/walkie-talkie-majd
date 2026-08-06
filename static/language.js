f24264275666cac265a74577c8a65efbaaef8d67
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-06 23:00:35 +0300

    Create language.js

---
+++ static/language.js
@@ -0,0 +1,20 @@
+const translations = {
+
+    ar: {
+        friends: "👥 الأصدقاء",
+        createRoom: "🎙️ إنشاء غرفة",
+        joinRoom: "🚪 دخول إلى غرفة",
+        publicRooms: "🌍 الغرف العامة",
+        settings: "⚙️ الإعدادات"
+    },
+
+
+    tr: {
+        friends: "👥 Arkadaşlar",
+        createRoom: "🎙️ Oda Oluştur",
+        joinRoom: "🚪 Odaya Katıl",
+        publicRooms: "🌍 Genel Odalar",
+        settings: "⚙️ Ayarlar"
+    }
+
+};
