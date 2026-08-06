81f3a6beedf0aae5217f1a6dd3ba80c53f8e4b44
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 02:31:54 +0300

    Update user.js

--- static/user.js
+++ static/user.js
@@ -59,3 +59,64 @@ onAuthStateChanged(auth, async (user)=>{
     }
 
 });
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    getDatabase,
+    ref,
+    get,
+    set
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+import { app } from "./app.js";
+
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+async function createUserID(user){
+
+    const userRef = ref(db, "users/" + user.uid);
+
+    const snapshot = await get(userRef);
+
+    if(!snapshot.exists()){
+
+        const allUsers = await get(ref(db, "users"));
+
+        let number = 1;
+
+        if(allUsers.exists()){
+            number = Object.keys(allUsers.val()).length + 1;
+        }
+
+        const customID = "MC-" + String(number).padStart(6,"0");
+
+        await set(userRef,{
+            id: customID,
+            name: user.displayName,
+            photo: user.photoURL
+        });
+
+    }
+
+}
+
+onAuthStateChanged(auth, async (user)=>{
+
+    if(!user) return;
+
+    await createUserID(user);
+
+    const snap = await get(ref(db,"users/"+user.uid));
+
+    if(snap.exists()){
+
+        const info = snap.val();
+
+        document.getElementById("userName").textContent = info.name;
+        document.getElementById("userPhoto").src = info.photo;
+        document.getElementById("userId").textContent = "ID : " + info.id;
+
+    }
+
+});
