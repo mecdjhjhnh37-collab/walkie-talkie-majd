2620e4be89fb74d9d1bedaa375b2efd71cb1135e
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 02:00:08 +0300

    Implement user ID creation on authentication
    Added user ID creation and database integration for new users.

--- static/user.js
+++ static/user.js
@@ -1,32 +1,113 @@
 import { getAuth, onAuthStateChanged } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+import { 
+    getDatabase,
+    ref,
+    get,
+    set
+} 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 import { app } from "./app.js";
+
 
 const auth = getAuth(app);
 
+const db = getDatabase(app);
 
-onAuthStateChanged(auth, (user)=>{
+
+
+async function createUserID(user){
+
+
+    const userRef = ref(db, "users/" + user.uid);
+
+
+    const snapshot = await get(userRef);
+
+
+
+    if(!snapshot.exists()){
+
+
+        const allUsers = await get(ref(db, "users"));
+
+
+        let number = 1;
+
+
+
+        if(allUsers.exists()){
+
+
+            number = Object.keys(allUsers.val()).length + 1;
+
+
+        }
+
+
+
+        const customID =
+        "MC-" + String(number).padStart(6,"0");
+
+
+
+        await set(userRef, {
+
+            id: customID,
+            name: user.displayName,
+            photo: user.photoURL
+
+        });
+
+
+    }
+
+
+}
+
+
+
+onAuthStateChanged(auth, async (user)=>{
 
 
     if(user){
 
 
-        document.getElementById("userName").textContent =
-        user.displayName;
+        await createUserID(user);
 
 
-        document.getElementById("userPhoto").src =
-        user.photoURL;
+
+        const data = await get(
+            ref(db, "users/" + user.uid)
+        );
 
 
-        document.getElementById("userId").textContent =
-        "ID : " + user.uid;
+
+        if(data.exists()){
 
 
-    } else {
+            const info = data.val();
 
-        console.log("لا يوجد مستخدم");
+
+
+            document.getElementById("userName").textContent =
+            info.name;
+
+
+
+            document.getElementById("userPhoto").src =
+            info.photo;
+
+
+
+            document.getElementById("userId").textContent =
+            "ID : " + info.id;
+
+
+        }
+
 
     }
 
