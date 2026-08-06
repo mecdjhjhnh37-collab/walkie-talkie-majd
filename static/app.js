1205038c8631b0badea158af4ee44571b6d5f843
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 01:14:58 +0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,22 +1,86 @@
-loginBtn.addEventListener("click", async () => {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-    alert("وصل للزر");
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-    try {
 
-        const result = await signInWithPopup(
-            auth,
-            provider
-        );
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyBrxgm0VQTfRv0ixXh9uQ81HBJ8SmD8c1E",
+    authDomain: "mecd-voice-ap.firebaseapp.com",
+    projectId: "mecd-voice-ap",
+    storageBucket: "mecd-voice-ap.firebasestorage.app",
+    messagingSenderId: "595103941809",
+    appId: "1:595103941809:web:e50577f156bcea3633ce90"
+};
 
-        alert("تم الدخول");
 
-        window.location.href="/home";
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-    } catch(error){
+const auth = getAuth(app);
 
-        alert(error.code + "\n" + error.message);
+const provider = new GoogleAuthProvider();
+
+
+
+// Google Login
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    const loginBtn = document.getElementById("loginBtn");
+
+
+    if (loginBtn) {
+
+
+        loginBtn.addEventListener("click", async () => {
+
+
+            alert("وصل للزر");
+
+
+            try {
+
+
+                const result = await signInWithPopup(
+                    auth,
+                    provider
+                );
+
+
+                alert("تم الدخول");
+
+
+                console.log(result.user);
+
+
+                window.location.href = "/home";
+
+
+            } catch(error) {
+
+
+                console.log(error);
+
+
+                alert(
+                    error.code +
+                    "\n\n" +
+                    error.message
+                );
+
+
+            }
+
+
+        });
+
 
     }
+
 
 });
