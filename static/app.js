9cbf88843664a11289a86a3dce4de974e2b49287
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 00:38:05 +0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -3,11 +3,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
     getAuth,
     GoogleAuthProvider,
-    signInWithPopup
+    signInWithRedirect,
+    getRedirectResult
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
-// Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBrxgm0VQTfRv0ixXh9uQ81HBJ8SmD8c1E",
     authDomain: "mecd-voice-ap.firebaseapp.com",
@@ -26,77 +26,40 @@ const provider = new GoogleAuthProvider();
 
 
 
-
-function loadLanguage(){
-
-    let lang = localStorage.getItem("language") || "ar";
-
-    const friends = document.getElementById("friendsText");
-    const create = document.getElementById("createRoomText");
-    const join = document.getElementById("joinRoomText");
-    const publicRooms = document.getElementById("publicRoomsText");
-    const settings = document.getElementById("settingsText");
-
-
-    if(friends)
-        friends.textContent = translations[lang].friends.replace("👥 ","");
-
-    if(create)
-        create.textContent = translations[lang].createRoom.replace("🎙️ ","");
-
-    if(join)
-        join.textContent = translations[lang].joinRoom.replace("🚪 ","");
-
-    if(publicRooms)
-        publicRooms.textContent = translations[lang].publicRooms.replace("🌍 ","");
-
-    if(settings)
-        settings.textContent = translations[lang].settings.replace("⚙️ ","");
-
-}
-
-
-document.addEventListener("DOMContentLoaded", loadLanguage);
+document.addEventListener("DOMContentLoaded", () => {
 
     const loginBtn = document.getElementById("loginBtn");
 
 
     if(loginBtn){
 
-        loginBtn.onclick = async () => {
+        loginBtn.addEventListener("click", () => {
 
+            signInWithRedirect(auth, provider);
 
-            try {
-
-                const result = await signInWithPopup(
-                    auth,
-                    provider
-                );
-
-
-                const user = result.user;
-
-
-                console.log("Login:", user);
-
-
-                window.location.href = "/home";
-
-
-            } catch(error) {
-
-
-                console.error(error);
-
-                alert(error.message);
-
-
-            }
-
-
-        };
+        });
 
     }
+
+
+
+    getRedirectResult(auth)
+    .then((result)=>{
+
+        if(result){
+
+            window.location.href="/home";
+
+        }
+
+    })
+    .catch((error)=>{
+
+        console.log(error);
+
+        alert(error.message);
+
+    });
 
 
 });
