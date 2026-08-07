1bca54bd422e44af84a1d65e18d4a98891b39204
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-07 16:52:41 +0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import {
     getAuth,
     GoogleAuthProvider,
